16f086368b6c20b4e26f81c8b54c2228f94163c5
Ayman Mustafa Mahdi <aymanmahdi20021510@gmail.com>
2026-01-12 23:47:33 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+/*1- function that result in the sum of 2 numbers*/
+function sum(a, b) {
+    let result = a + b;
+    console.log(result);
+    sum(10, 20);
+}
+/*sum(10, 20);*/
+
+/*2- function that result in the multiplication of 2 numbers*/
+function multiply(a, b) {
+    let result = a * b;
+    console.log(result);
+}
+/*multiply(5, 4);*/
+
